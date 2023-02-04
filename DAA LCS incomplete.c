f3874b1d7fb5c1e984cd2a012e12f99fc756a1b1
Deepthi B <91285032+deepthi1129@users.noreply.github.com>
2023-02-04 14:57:54 +0530

    Stack smashed the code :(
    You need to figure it out by yourself (ps: use ChatGPT)

---
+++ DAA LCS incomplete.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+
+void main(){
+    // received inputs
+    char str1[6];
+    char str2[6];
+    int top,left;
+    printf("Enter str1: \n");
+    scanf("%s",&str1);
+    printf("Enter str2: \n");
+    scanf("%s",&str2);
+    //printf("%s,%s",str1,str2);
+    //received string inputs successfully
+    int matrix[6][6];
+    for(int i=0;i<=6;i++){
+        matrix[i][0]=0;
+        matrix[0][i]=0;
+    }
+    for(int i=1;i<=6;i++){
+        for(int j=1;j<=6;j++){
+            if (str1[i]==str2[j])
+                matrix[i][j]=matrix[i-1][j-1];
+            else
+                left=matrix[i][j-1];
+                top=matrix[i-1][j];
+                if (left>top)
+                matrix[i][j]=left;
+                else
+                matrix[i][j]=top;
+            printf("%d",matrix[i][j]);
+            }
+        }
+    }
