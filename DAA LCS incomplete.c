1c67bf6fd6b88235cb898ffd8dcdef644253ae63
Deepthi B <91285032+deepthi1129@users.noreply.github.com>
2023-02-04 15:01:39 +0530

    increased 2d array size

--- DAA LCS incomplete.c
+++ DAA LCS incomplete.c
@@ -12,23 +12,24 @@ void main(){
     scanf("%s",&str2);
     //printf("%s,%s",str1,str2);
     //received string inputs successfully
-    int matrix[6][6];
+    int matrix[7][7];
     for(int i=0;i<=6;i++){
         matrix[i][0]=0;
         matrix[0][i]=0;
     }
     for(int i=1;i<=6;i++){
         for(int j=1;j<=6;j++){
-            if (str1[i]==str2[j])
+            if (str1[i-1]==str2[j-1])
                 matrix[i][j]=matrix[i-1][j-1];
             else
-                left=matrix[i][j-1];
+                {left=matrix[i][j-1];
                 top=matrix[i-1][j];
                 if (left>top)
                 matrix[i][j]=left;
                 else
                 matrix[i][j]=top;
-            printf("%d",matrix[i][j]);
             }
+            printf("%d",matrix[i][j]);
         }
     }
+}
